f2132e0e9d89fed484289885784aeadc77e7ec69
GABRIEL DE MORAIS <gmr_3@hotmail.com>
2024-09-05 20:59:05 -0300

    Docs: Mudança na documentação

--- src/js/content.js
+++ src/js/content.js
@@ -1,1 +1,13 @@
-document.body.style.filter = 'brightness(0.7) contrast(1.2)';
+chrome.storage.sync.get(['darkMode'], function(result) {
+    if (result.darkMode) {
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+    }
+  });
+  
+  function toggleDarkMode() {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'black' ? '' : 'black';
+    document.body.style.color = document.body.style.color === 'white' ? '' : 'white';
+    chrome.storage.sync.set({ darkMode: document.body.style.backgroundColor === 'black' });
+  }
+  
